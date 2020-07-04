de458d00deaffc60c881fc156ecf8dd45d6a1ea8
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-07-04 20:48:16 +0100

    Initialized frontend with Create-React-App and created some pages

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+import NavBar from "./components/Navbar"
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Profile from "./pages/profile"
+import Signup from "./pages/signup"
+import "./App.css"
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
