3d07c73fbb4260397113af766795b50988e364e1
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-07-04 10:31:28 +0100

    Encryted password with bcryptjs npm package

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
+const bcrypt = require('bcryptjs')
 
 router.get('/', (req, res) => res.send("HELLO!"))
 
@@ -15,16 +16,20 @@ router.post('/signup', (req, res) => {
             if (savedUser) {
                 return res.status(422).json({ error: "user already exist with that email" })
             }
-            const user = new User({
-                email,
-                password,
-                name
-            })
-            user.save()
-                .then(user => {
-                    res.json({ message: "saved successfully" })
+            bcrypt.hash(password, 12)
+                .then(hashedpassword => {
+                    const user = new User({
+                        email,
+                        password: hashedpassword,
+                        name
+                    })
+                    user.save()
+                        .then(user => {
+                            res.json({ message: "saved successfully" })
+                        })
+                        .catch(err => console.log(err))
                 })
-                .catch(err => console.log(err))
+
         })
         .catch(err => console.log(err))
 })
