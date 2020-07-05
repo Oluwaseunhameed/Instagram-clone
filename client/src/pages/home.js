9c21530f31ec154eb096c1a800ef40c0a21a31d4
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-07-05 13:33:20 +0100

    Home page layout added

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,7 +2,44 @@ import React from "react"
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Oluwaseun</h5>
+                <div className="card-image">
+                    <img src="https://images.wallpapersden.com/image/download/4k-colorful-landscape_68515_3840x2160.jpg" alt="wallpaper" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" >favorite</i>
+                    <h6>title</h6>
+                    <p>this is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Oluwaseun</h5>
+                <div className="card-image">
+                    <img src="https://images.wallpapersden.com/image/download/4k-colorful-landscape_68515_3840x2160.jpg" alt="wallpaper" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" >favorite</i>
+                    <h6>title</h6>
+                    <p>this is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Oluwaseun</h5>
+                <div className="card-image">
+                    <img src="https://images.wallpapersden.com/image/download/4k-colorful-landscape_68515_3840x2160.jpg" alt="wallpaper" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" >favorite</i>
+                    <h6>title</h6>
+                    <p>this is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+        </div>
     )
 }
 
